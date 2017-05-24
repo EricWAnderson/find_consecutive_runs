4ddecfa9a91f403a1c391545451dfadfbbff90ec
Eric Anderson <eric@diviup.org>
2017-05-23 19:46:37 -0500

    fix test

--- spec/consecutive_runs_spec.rb
+++ spec/consecutive_runs_spec.rb
@@ -15,12 +15,12 @@ end
 describe '#negative_run' do
   it 'should return true if 3 consecutive numbers that decrease by 1' do
     array = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7]
-    expect(positive_run(array, 0)).to eq true
+    expect(negative_run(array, 4)).to eq true
   end
 
   it 'should return false if 3 consecutive numbers do not decrease by 1' do
     array = [1, 2, 1, 5, 10, 1, 8, 1, 10, 11, 7, 8, 7]
-    expect(positive_run(array, 0)).to eq false
+    expect(negative_run(array, 0)).to eq false
   end
 end
 
